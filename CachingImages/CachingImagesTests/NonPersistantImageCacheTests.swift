20685a807c86be30d99d708599b289fd8ced7d39
surendrak11 <surendrak11@gmail.com>
2019-05-10 16:58:35 +0100

    Added some tests

--- CachingImages/CachingImagesTests/NonPersistantImageCacheTests.swift
+++ CachingImages/CachingImagesTests/NonPersistantImageCacheTests.swift
@@ -16,24 +16,27 @@ class NonPersistantImageCacheTests: XCTestCase {
     
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        imageCache?.removeAllCachedItem()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testCachingImageIntoNonPersistantImageCache() {
+    func test_Caching_Image_Into_NonPersistantImageCache() {
         let imageToBeChached = #imageLiteral(resourceName: "TestImage")
         imageCache?.cacheItem(imageToBeChached, forKey: imageCacheKey)
         let retrievedImage = self.imageCache?.cachedItemForKey(self.imageCacheKey)
         XCTAssertEqual(imageToBeChached, retrievedImage, "Chached image should be equal to retrieved image")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_Remvoe_All_Cached_Image_From_NonPersistantImageCache() {
+        let imageToBeChached = #imageLiteral(resourceName: "TestImage")
+        imageCache?.cacheItem(imageToBeChached, forKey: imageCacheKey)
+        imageCache?.removeAllCachedItem()
+        let retrievedImage = self.imageCache?.cachedItemForKey(self.imageCacheKey)
+        XCTAssertEqual(retrievedImage, nil, "Cache should be empty")
     }
+
 
 }
